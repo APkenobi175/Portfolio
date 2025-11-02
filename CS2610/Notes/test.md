eba947a3ed8d69c07a5c143587e56e1163826b24
Ammon <157701457+APkenobi175@users.noreply.github.com>
2025-11-02 10:31:59 -0700

    more notes

---
+++ CS2610/Notes/test.md
@@ -0,0 +1,29 @@
+* Hello
+
+### Hello
+
+# Hello
+
+1. Hello
+
+**Hello**
+
+```python
+print ("Hello World")
+
+for i in range (3):
+	for j in range (3):
+		print("Hello world")
+
+def add(x,y):
+	return x + y
+
+add(1,2)
+
+```
+
+```
+fdjksald;jfasld
+sdakfl;jasdklfasd
+```
+
--- CS2810/Assignments/Assignment7/fib.asm
+++ CS2810/Assignments/Assignment7/fib.asm
@@ -1,8 +1,9 @@
 .data 
+.align 2
 array: .space 128 #(32 words = 32*4 = 128)
 prompt: .asciiz "Please Enter How Many Fibonacci Numbers You Would Like:  "
 newLine: .asciiz "\n"
-comma: .asciiz ", "
+space: .asciiz " "
 
 
 
@@ -29,7 +30,9 @@ Loop:
 	slt $t4, $s1, $s2 
 	beq $t4, $zero, LoopDone # if i>=n you are done
 	
-	sw $s1, 0($s3)
+	move $a0, $s1
+	jal Fib
+	sw $v0, 0($s3)
 	
 	# print first i+1 elements
 	move $a0, $s0 # base address
@@ -71,15 +74,15 @@ PrntArrLoop:
 	li $v0, 1 # print int
 	syscall
 	
-	# if you are at the lest element, don't print the comma
-	beq $t1, $t3, skipComma
+	# if you are at the lest element, don't print space
+	beq $t1, $t3, skipSpace
 	
 	# print comma
 	li $v0, 4 # print string
-	la $a0, comma
+	la $a0, space
 	syscall
 	
-skipComma:
+skipSpace:
 	addi $t1, $t1, 1 # i++
 	addi $t0, $t0, 4 # increment offset
 	j PrntArrLoop # loop until done
@@ -97,5 +100,55 @@ PrntArrDone:
 	jr $ra # go back to ra
 ### END PRINT ARRAY FUNCTION ###
 
+
+### FIB FUNCTION ###
+Fib:
+	addi $sp, $sp, -12 # add some space in the stack
+	sw $ra, 8($sp) # save ra
+	sw $a0, 0($sp) # save n
+	
+	# if n==0 return 0 base case 1
+	beq $a0, $zero, fibZero
+
+	# if n == 1 return 1 base case 2
+	
+	li $t0, 1
+	beq $a0, $t0, fibOne
+	
+	# Else do recursive case
+	# find fib(n-1)
+	addi $a0, $a0, -1
+	jal Fib
+	sw $v0, 4($sp) # save fib(n-1)
+	
+	
+	# find fib (n-2)
+	lw $a0, 0($sp) # restore original n
+	addi $a0, $a0, -2
+	jal Fib
+	
+	# do fib(n-1) + fib(n-2)
+	lw $t1, 4($sp) 
+	add $v0, $v0, $t1 # return register 0 = fib(n-2) + fib(n-1)
+	j fibDone
+
+	
+fibZero:
+	# Return 0
+	li $v0, 0
+	j fibDone 
+fibOne:
+	# Return 1
+	li $v0, 1
+	j fibDone
+	
+fibDone:
+	# Restore the stack and return to ra
+	lw $a0, 0($sp)
+	lw $ra, 8($sp)
+	addi $sp, $sp, 12
+	jr $ra
+	
+### END FIB FUNCTION ###
 
 	
