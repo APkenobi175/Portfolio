d9c685a30e6aa013c5de67481d735c6ff6d061dc
APKenobi175 <phippsammon175@gmail.com>
2026-02-02 14:43:25 -0700

    Talked about continued fractions

---
+++ CS3430/Notes/Practice Scripts/2-2-26.py
@@ -0,0 +1,11 @@
+x = "Hello World"
+
+print (x[:-2])
+
+# prints out H without the ello World at the end
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print (x[:-3])
+
+# prints out [1, 2, 3, 4, 5, 6, 7] without the 8, 9, 10 at the end
